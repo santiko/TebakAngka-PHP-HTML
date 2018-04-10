00634623bf06a8aa676614cdc7061c2cfc65e44d
santiko <samokit@gmail.com>
2018-04-10 23:54:32 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>.:: TEBAK ANGKA @ 2018 ::.</title>
+
+<body>
+<center>
+	<h3>APLIKASI</h3>
+	<img src="sirah.gif">
+	<h1>"<u>TEBAK ANGKA</u>"</h1>
+	<h3><i>Berbasis bahasa pemrograman HTML dan PHP</i></h3>
+		<form action="" method="POST">
+		<input type="text" value="" name="txtAngka" placeholder="Angka antara 1 sampai 5..." autofocus/>
+	</form>
+	<h2>Sip dech...!<br>Anda memilih angka <font color="green"> [5] </font> sebagai pilihan Anda..!</h2>	<small>Dikembangkan oleh: <br><font color="green"><b>SANTIKO KUSNUL HAKIM</font><br></b>pada tanggal</font><b><br>- 9 April 2018 -<i></b><br>.. dengan seksama pada tahap <b>try-and-error</b> ..<br>.. dalam tempo yang sesingkat-singkatnya ..</i></small>
+	</center>
+</body>
+</head>
+</html>
